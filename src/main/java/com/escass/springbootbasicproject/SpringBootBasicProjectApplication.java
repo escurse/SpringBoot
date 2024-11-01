0a72ee8cc3eaa9f94c36e7f0b3937998ce62b13d
escurse <av1967@naver.com>
2024-11-01 09:19:46 +0900

    SBBP

---
+++ src/main/java/com/escass/springbootbasicproject/SpringBootBasicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.escass.springbootbasicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBasicProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/escass/springbootbasicproject/SpringBootBasicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escass.springbootbasicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
