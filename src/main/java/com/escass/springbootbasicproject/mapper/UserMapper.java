593ad2f367a79351efce40c101235df053f4380c
escurse <av1967@naver.com>
2024-11-01 17:26:44 +0900

    SBBP

--- src/main/java/com/escass/springbootbasicproject/mapper/UserMapper.java
+++ src/main/java/com/escass/springbootbasicproject/mapper/UserMapper.java
@@ -17,18 +17,18 @@ public interface UserMapper {
     Date selectNow();
 
     // 모든 유저를 DB에서 조회함
-    @Select("SELECT * FROM basic.user")
+    @Select("SELECT * FROM user")
     List<User> selectUsers();
 
     // 아이디를 기반으로 유저 한 명만 조회하기
-    @Select("SELECT * FROM basic.user WHERE id = #{userID}")
-    User selectUserById(String userId);
+    @Select("SELECT * FROM user WHERE id = #{userID}")
+    User selectUserById(String userID);
 
     // 아이디와 패스워드를 기반으로 유저 한 명을 조회하기
     @Select("SELECT * FROM user WHERE id = #{userID} AND password = #{password}")
     User selectUser(String userID, String password);
 
     // MyBatis는 Getter를 사용해서 객체의 변수 값을 가져가서 쓴다
-    @Insert("INSERT INTO `basic`.`user` VALUES (#{id}, #{password}, #{nickName}, now())")
+    @Insert("INSERT INTO `user` VALUES (#{id}, #{password}, #{nickname}, now())")
     void insertUser(User user);
 }
