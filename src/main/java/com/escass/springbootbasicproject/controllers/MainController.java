7eb67205131dc8310fd11fadbe8d37bd9e568404
escurse <av1967@naver.com>
2024-11-01 12:49:21 +0900

    SBBP

--- src/main/java/com/escass/springbootbasicproject/controllers/MainController.java
+++ src/main/java/com/escass/springbootbasicproject/controllers/MainController.java
@@ -1,15 +1,22 @@
 package com.escass.springbootbasicproject.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
     @RequestMapping("/board") // 주소창에 적는 경로
-    ModelAndView get_board(){
+    ModelAndView get_board(HttpSession session){
         System.out.println("get_board가 실행됨");
+        Object loginData = session.getAttribute("login");
+        if(loginData == null){
+            System.out.println("로그인 안하고 왔네?");
+            return new ModelAndView("user/login");
+        }
         ModelAndView mav = new ModelAndView();
         // prefix: 앞에 붙는 단어 => /templates/
         // suffix: 뒤에 붙는 단어 => .html
@@ -29,4 +36,8 @@ public class MainController {
         // String 형태를 반환하면 해당 이름의 html을 찾아간다!
         return "main/write";
     }
+
+
+
+
 }
--- src/main/java/com/escass/springbootbasicproject/controllers/UserController.java
+++ src/main/java/com/escass/springbootbasicproject/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.escass.springbootbasicproject.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,14 +14,22 @@ public class UserController {
     // 반환 안해도 알아서 url 경로와 같은 html 찾아감
     @GetMapping("/login")
     public void get_login() {
+        System.out.println("get_login이 실행됨");
     }
 
     @PostMapping("/login")
     String post_login(
-            @RequestParam("id") String userID,
-            @RequestParam("pw") String userPW
+            @RequestParam("id") String userID, // 파라미터
+            @RequestParam("pw") String userPW, // 파라미터
+            HttpSession session // 현재 어플리케이션에서 사용하는 세션
     ) {
+        // id: korea / pw: 123
+        System.out.println("post_login이 실행됨");
+        System.out.println("받은id: " + userID);
+        System.out.println("받은pw: " + userPW);
+        // 로그인 성공이다!!
         if(userID.equals("korea") && userPW.equals("123")) {
+            session.setAttribute("login", true);
             // redirect: 재 요청을 보낸다 (GET)
             // url 적는 곳의 경로를 적어야 함!
             // '/' 를 작성하면 localhost:8080 이 기준
@@ -30,6 +39,13 @@ public class UserController {
         }
         return "redirect:/user/login";
     }
+
+
+
+
+
+
+
 
     @GetMapping("/register")
     public void get_register() {}
