f8bc067196d2cbebcf58cb1415b8821e9ea6881a
escurse <av1967@naver.com>
2024-11-01 10:26:25 +0900

    SBBP

--- src/main/java/com/escass/springbootbasicproject/controllers/UserController.java
+++ src/main/java/com/escass/springbootbasicproject/controllers/UserController.java
@@ -3,13 +3,18 @@ package com.escass.springbootbasicproject.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value="/user")
 public class UserController {
     @RequestMapping(value="/login", method = RequestMethod.GET)
-    public ModelAndView getLogin() {
+    @ResponseBody
+    public ModelAndView getLogin(@RequestParam(value="id", required = false) String username, @RequestParam(value="pw", required = false) String password) {
+        System.out.println(username);
+        System.out.println(password);
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("user/login");
         return modelAndView;
